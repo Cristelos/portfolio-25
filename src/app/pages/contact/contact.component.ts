a82ef0bdb638be422bc7071fbdc32ad60a0628e9
Cristelos <cristelos@msn.com>
2025-06-18 18:21:52 +0200

    (feature) => finished about and contact page

--- src/app/pages/contact/contact.component.ts
+++ src/app/pages/contact/contact.component.ts
@@ -1,12 +1,22 @@
 import { CommonModule } from '@angular/common';
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ButtonCustomComponent } from '../../shared/components/button-custom/button-custom.component';
+import { DotsComponent } from '../../shared/decorations/dots/dots.component';
+import { WaveComponent } from '../../shared/decorations/wave/wave.component';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-contact',
-  standalone:true,
-  imports: [CommonModule],
+  standalone: true,
+  imports: [
+    CommonModule,
+    ButtonCustomComponent,
+    DotsComponent,
+    WaveComponent,
+    MatIconModule,
+  ],
   templateUrl: './contact.component.html',
   styleUrl: './contact.component.css',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export default class ContactComponent { }
+export default class ContactComponent {}
